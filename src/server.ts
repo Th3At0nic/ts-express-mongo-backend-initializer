64dc98eea31fe1c795cd119e3d7a7d65e7a99fd9
Md Rahatul Islam <islammdrahatul@gmail.com>
2025-03-26 00:18:42 +0600

    Updated server.ts manually

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,36 @@
+import { Server } from 'http';
 import app from './app';
 import config from './app/config';
 import mongoose from 'mongoose';
 
+let server: Server;
+
 async function main() {
-  await mongoose.connect(config.database_url as string);
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    server = app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-app.listen(config.port, () => {
-  console.log(`Example app listening on port: ${config.port}`);
+main();
+
+process.on('unhandledRejection', () => {
+  console.log(`An unhandledRejection detected! Shutting down...`);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
 });
 
-main();
+process.on('uncaughtException', () => {
+  console.log('An uncaughtException detected! Shutting down...');
+  process.exit(1);
+});
