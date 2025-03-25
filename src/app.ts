1bc0f0d52c9969d2b0a88653edfffcd0b4d2c976
Md Rahatul Islam <islammdrahatul@gmail.com>
2025-03-26 00:18:06 +0600

    Updated app.ts manually

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import router from './app/routes';
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import { notFound } from './app/middlewares/notFound';
 const app: Application = express();
 
-///parser
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+
+app.use('/api', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+//this is the global error handler
+app.use(globalErrorHandler);
+
+// not found handler
+app.use(notFound);
 
 export default app;
